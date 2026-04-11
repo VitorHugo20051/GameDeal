2efd04a1dd144a3d438ff5c1f20f2199d3a096f4
VitorHugo20051 <vitorhugoplayer@gmail.com>
2026-04-11 22:10:53 +0100

    feat: setup sinatra app with session and health endpoint

---
+++ gamedeal_api/app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'json'
+require_relative 'middleware/auth_guard'
+require_relative 'db'
+require_relative 'routes/auth'
+
+enable :sessions
+set :session_secret, ENV['SESSION_SECRET'] || 'dev_secret_muito_longo_para_desenvolvimento_local_muda_em_producao_obrigatorio'
+
+get '/health' do
+  content_type :json
+  { status: 'ok' }.to_json
+end
