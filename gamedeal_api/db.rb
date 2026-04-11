22e64d891c89da9cd314f7633be26949b434d4a0
VitorHugo20051 <vitorhugoplayer@gmail.com>
2026-04-11 22:10:41 +0100

    db: add postgresql connection

---
+++ gamedeal_api/db.rb
@@ -0,0 +1,11 @@
+require 'pg'
+require 'dotenv/load'
+
+def db
+  @db ||= PG.connect(
+    host: ENV['DB_HOST'] || 'localhost',
+    dbname: ENV['DB_NAME'] || 'gamedeal_db',
+    user: ENV['DB_USER'] || 'postgres',
+    password: ENV['DB_PASSWORD'] || 'admin'
+  )
+end
