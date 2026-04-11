ae136c31690d87c72504340a4fa9a6e3596bc2ec
VitorHugo20051 <vitorhugoplayer@gmail.com>
2026-04-11 22:11:05 +0100

    feat: add user model with bcrypt authentication

---
+++ gamedeal_api/models/user.rb
@@ -0,0 +1,24 @@
+require 'bcrypt'
+require_relative '../db'
+
+class User
+  def self.create(email, password)
+    password_hash = BCrypt::Password.create(password)
+    db.exec_params(
+      'INSERT INTO users (email, password_hash, provider) VALUES ($1, $2, $3) RETURNING id, email',
+      [email, password_hash, 'local']
+    ).first
+  rescue PG::UniqueViolation
+    nil
+  end
+
+  def self.find_by_email(email)
+    db.exec_params('SELECT * FROM users WHERE email = $1', [email]).first
+  end
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    return nil unless user
+    BCrypt::Password.new(user['password_hash']) == password ? user : nil
+  end
+end
